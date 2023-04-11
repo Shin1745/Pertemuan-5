9dd5cf81e2669f922d97805fb0c34f9cb8121fb0
Shin1745 <127808802+Shin1745@users.noreply.github.com>
2023-04-11 11:40:38 +0800

    Add files via upload

---
+++ sesi5/koneksidb.php
@@ -0,0 +1,18 @@
+<?php
+    include("konfigurasi.php");
+
+    $cnn = mysqli_connect(DBHOST, DBUSER, DBPASS,); 
+    if($cnn){
+       echo "Koneksi Sukses";
+       $sql1 = "CREATE DATABASE " . DBNAME;
+
+       $hasil = mysqli_query($cnn, $sql1);
+
+       if($hasil){
+        echo "Database" . DBNAME . "berhasil dibuat";
+       } else{
+        echo "Database" . DBNAME . "gagal dibuat";
+       }
+     } else{
+        echo "Koneksi Gagal" . mysqli_connect_error();
+     }
